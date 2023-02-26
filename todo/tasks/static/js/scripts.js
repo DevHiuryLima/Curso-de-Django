3efad0010662bc3448fdeab406cdfd93c3e4e808
Hiury Lima <limahiury6@gmail.com>
2023-02-26 17:18:11 -0300

    Adicionando funcionalidade de paginação e vericação na hora de deletar uma tarefa

--- todo/tasks/static/js/scripts.js
+++ todo/tasks/static/js/scripts.js
@@ -1,1 +1,21 @@
-console.log('Funcionou');
+$( document ).ready(function() {
+
+    var baseUrl   = 'http://localhost:8000/';
+    var deleteBtn = $('.delete-btn');
+    var searchBtn = $('#search-btn');
+    var searchForm = $('#search-form');
+    var filter     = $('#filter');
+    
+    $(deleteBtn).on('click', function(e) {
+
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar esta tarefa?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+
+    });
+});
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
 from django.contrib import messages
@@ -7,7 +8,14 @@ from .models import Task
 
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 10)
+
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
+
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 
