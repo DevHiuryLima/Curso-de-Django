f2fa996544308abf6541512fb5d6f903a4459ed2
Hiury Lima <limahiury6@gmail.com>
2023-02-27 12:38:51 -0300

    Adicionando funcionalidade de concluir tarefa e corrigindo erro no model de Task

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -16,7 +16,7 @@ class Task(models.Model):
     )
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
