317613535d7e598019800fd91778cafb11da55e2
Hiury Lima <limahiury6@gmail.com>
2023-02-27 00:16:27 -0300

    Autenticação do projeto. Criando as funcionalidades de login e logout.

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
@@ -7,6 +8,7 @@ from django.contrib import messages
 from .models import Task
 
 
+@login_required
 def taskList(request):
 
     search = request.GET.get('search')
@@ -25,11 +27,13 @@ def taskList(request):
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
 
 
+@login_required
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -45,6 +49,7 @@ def newTask(request):
         return render(request, 'tasks/addtask.html', {'form': form})
 
 
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -61,6 +66,7 @@ def editTask(request, id):
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
 
 
+@login_required
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
@@ -68,6 +74,7 @@ def deleteTask(request, id):
     messages.info(request, 'Tarefa deletada com sucesso.')
 
     return redirect('/')
+
 
 def helloworld(request):
     return HttpResponse('Hello World!')
